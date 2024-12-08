73a2b2bbf88ff58fe57403a7811f2b3dcedac790
Hilal Muhammad <155317768+ihilalmuhammad@users.noreply.github.com>
2024-12-08 12:46:12 +0500

    Create Test.js

---
+++ 01_Basic/Test.js
@@ -0,0 +1,14 @@
+//  Number Guessing Game 
+let GamNum = 50;
+let UserNum = prompt("Guess the Game Number")
+while (UserNum != GamNum) {
+    UserNum = prompt("U guess The Wrong Number Please Enter The Correct")
+}
+
+alert("Congratulation! U Guess The Correct Number.")
+
+
+
+
+
+
