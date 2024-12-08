145bd2732deb3b8ffc1c14ef1526fab66e0d4716
Hilal Muhammad <155317768+ihilalmuhammad@users.noreply.github.com>
2024-12-08 15:03:49 +0500

    Update and rename 01_varibales.js to 01_variables.js

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,11 @@
+// Explanation 
+// when we use the var they can show some disadvantages bcs they use at the same role under the scope or out of the scope 
+// so we use the let Bassicaly 
+const Id = 1234
+let Name = "Hilal"
+var address = "Charsadda"
+study = "RIUMC"
+
+console.table([Id,Name,address,study])
+
+
--- 01_Basic/01_varibales.js
+++
@@ -1,7 +0,0 @@
-const Id = 1234
-let Name = "Hilal"
-var address = "Charsadda"
-study = "RIUMC"
-
-console.table([Id,Name,address,study])
-
