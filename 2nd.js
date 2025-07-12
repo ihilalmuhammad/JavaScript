08500e0abcf4374542fed237244579cabc3bc3ab
Hilal Muhammad <155317768+ihilalmuhammad@users.noreply.github.com>
2025-07-12 10:31:18 +0500

    variable-declaration-and-data-types.js

---
+++ 2nd.js
@@ -0,0 +1,17 @@
+// variable declaration
+var name = "John";
+let age = 30;
+//cpmparison b/t var and let
+// var can be used globally in code and locally while let can only be used locally in blockof code  
+const country = "USA"; // constant variable, cannot be reassigned
+
+// // different data types in javascript
+var isStudent = true; // boolean
+let height = 5.9; // float
+let a = "harry"; // string
+let b = null; // null
+let c = undefined; // undefined
+let r = Symbol(); // symbol
+
+console.log(name, age, country, isStudent, height, a, b, c, r);
+console.log(typeof name, typeof age, typeof country, typeof isStudent, typeof height, typeof a, typeof b, typeof c, typeof r);
