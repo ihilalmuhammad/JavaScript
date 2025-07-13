fb551305c9f98d70d7adc73cb636c91c5f4ffa5b
Hilal Muhammad <155317768+ihilalmuhammad@users.noreply.github.com>
2025-07-13 12:40:39 +0500

    Condition if /else

---
+++ 3rd.js
@@ -0,0 +1,11 @@
+// if (condition) {
+//     // code to execute if condition is true
+// } else {
+//     // code to execute if condition is false
+// }
+let age = 20;
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+}
