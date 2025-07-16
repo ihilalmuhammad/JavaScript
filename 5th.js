98e71637c2b2fc6b7f64dfa2f8fc9bdf8b566aab
Hilal Muhammad <155317768+ihilalmuhammad@users.noreply.github.com>
2025-07-16 08:05:16 +0500

    function

---
+++ 5th.js
@@ -0,0 +1,19 @@
+function nice(name) {
+    console.log("hello i " + name + " from akhoondheri " );
+     console.log("hello i " + name + " from akhoondheri " );
+      console.log("hello i " + name + " from akhoondheri " );
+
+}
+nice("Hilal")
+nice("Ali")
+
+function mul(a, b) {
+    console.log(a * b)
+    
+}
+mul(2,5)
+   
+function sum(a,b) {
+    console.log(a+b)
+}
+sum(23,45)
