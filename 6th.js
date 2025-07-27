2ca749c18219a7d6590d332080653bd757d466c9
Hilal Muhammad <155317768+ihilalmuhammad@users.noreply.github.com>
2025-07-27 12:35:08 +0500

    String

---
+++ 6th.js
@@ -0,0 +1,12 @@
+console.log("this is string file 6th.js");
+
+let a = "hilal";
+console.log(a[0]); // h
+console.log(a[1]); // i 
+console.log(a[2]); // l
+console.log(a[3]); // a
+console.log(a[4]); // l
+console.log(a[5]); // undefined
+console.log(a.length); // 5
+console.log(a.replace)
+
